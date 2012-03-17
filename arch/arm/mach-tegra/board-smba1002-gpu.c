62599b3c61292b84d48488b303c5e0f7d1748b4d
Andrew Foss <fosser2@gmail.com>
2012-03-16 18:14:50 -0700

    Improved Background Banding (thanks mrdeadlocked)

--- arch/arm/mach-tegra/board-smba1002-gpu.c
+++ arch/arm/mach-tegra/board-smba1002-gpu.c
@@ -226,7 +226,9 @@ static struct tegra_dc_out smba1002_disp1_out = {
         /* Enable dithering. Tegra also supports error
                diffusion, but when the active region is less
                than 640 pixels wide. */
-       .dither         = TEGRA_DC_ERRDIFF_DITHER,
+       //.dither         = TEGRA_DC_ERRDIFF_DITHER,
+	     .depth          =  18,
+		 .dither         = TEGRA_DC_ORDERED_DITHER,
 
 	.height 	= 136, /* mm */
 	.width 		= 217, /* mm */
