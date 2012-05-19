840e7e947ffedd1deee4829679cd97b05d1c81a0
Andrew Foss <fosser2@gmail.com>
2012-05-18 23:12:57 -0500

    Stupid typo fixes

--- arch/arm/mach-tegra/board-smba1002-i2c.c
+++ arch/arm/mach-tegra/board-smba1002-i2c.c
@@ -1,7 +1,7 @@
 /*
  * arch/arm/mach-tegra/board-smba1002-i2c.c
  *
- * Copyright (C) 2011 Eduardo Jos� Tagle <ejtagle@tutopia.com>
+ * Copyright (C) 2011 Eduardo Jose Tagle <ejtagle@tutopia.com>
  * Copyright (C) 2011 Jens Andersen <jens.andersen@gmail.com>
  *
  * This software is licensed under the terms of the GNU General Public
@@ -36,7 +36,7 @@
 #include "gpio-names.h"
 #include "board-smba1002.h"
 
-static struct tegra_i2c_platform_data adam_i2c1_platform_data = {
+static struct tegra_i2c_platform_data smba1002_i2c1_platform_data = {
 .adapter_nr = 0,
 .bus_count = 1,
 .bus_clk_rate = { 100000, 0 },
@@ -52,7 +52,7 @@ static const struct tegra_pingroup_config i2c2_gen2 = {
 .func = TEGRA_MUX_I2C2,
 };
 
-static struct tegra_i2c_platform_data adam_i2c2_platform_data = {
+static struct tegra_i2c_platform_data smba1002_i2c2_platform_data = {
 .adapter_nr = 1,
 .bus_count = 2,
 .bus_clk_rate = { 100000, 100000 },
@@ -60,13 +60,13 @@ static struct tegra_i2c_platform_data adam_i2c2_platform_data = {
 .bus_mux_len = { 1, 1 },
 };
 
-static struct tegra_i2c_platform_data adam_i2c3_platform_data = {
+static struct tegra_i2c_platform_data smba1002_i2c3_platform_data = {
 .adapter_nr = 3,
 .bus_count = 1,
 .bus_clk_rate = { 100000, 0 },
 };
 
-static struct tegra_i2c_platform_data adam_dvc_platform_data = {
+static struct tegra_i2c_platform_data smba1002_dvc_platform_data = {
 .adapter_nr = 4,
 .bus_count = 1,
 .bus_clk_rate = { 100000, 0 },
@@ -75,10 +75,10 @@ static struct tegra_i2c_platform_data adam_dvc_platform_data = {
 
 int __init smba1002_i2c_register_devices(void)
 {
-tegra_i2c_device1.dev.platform_data = &adam_i2c1_platform_data;
-tegra_i2c_device2.dev.platform_data = &adam_i2c2_platform_data;
-tegra_i2c_device3.dev.platform_data = &adam_i2c3_platform_data;
-tegra_i2c_device4.dev.platform_data = &adam_dvc_platform_data;
+tegra_i2c_device1.dev.platform_data = &smba1002_i2c1_platform_data;
+tegra_i2c_device2.dev.platform_data = &smba1002_i2c2_platform_data;
+tegra_i2c_device3.dev.platform_data = &smba1002_i2c3_platform_data;
+tegra_i2c_device4.dev.platform_data = &smba1002_dvc_platform_data;
 
 platform_device_register(&tegra_i2c_device1);
 platform_device_register(&tegra_i2c_device2);
