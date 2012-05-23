60e77914d7c750ff1ad6d83c07e1c50219cb18a0
Andrew Foss <fosser2@gmail.com>
2012-05-22 20:33:48 -0500

    SMBA1002: vdd_fuse fix

--- arch/arm/mach-tegra/board-smba1002-power.c
+++ arch/arm/mach-tegra/board-smba1002-power.c
@@ -173,7 +173,8 @@ static struct regulator_consumer_supply tps658621_ldo6_supply[] = {
 */
 static struct regulator_consumer_supply tps658621_ldo7_supply[] = {
 	//REGULATOR_SUPPLY("vdd_ldo7", NULL),
-	REGULATOR_SUPPLY("avdd_hdmi", NULL)
+	REGULATOR_SUPPLY("avdd_hdmi", NULL),
+	REGULATOR_SUPPLY("vdd_fuse", NULL),
 };
 
 /* PLLHD voltage rail (HDMI) : AVDD_HDMI_PLL -> LDO8
